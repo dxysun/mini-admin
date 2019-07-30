e835d915e742954a21b7af4572d0a40364d4ef34
corn1ng <wangkangning1@foxmail.com>
2019-07-30 09:56:53 +0800

    init

---
+++ src/main/java/com/netease/miniadmin/MiniAdminApplication.java
@@ -0,0 +1,13 @@
+package com.netease.miniadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniAdminApplication.class, args);
+    }
+
+}
